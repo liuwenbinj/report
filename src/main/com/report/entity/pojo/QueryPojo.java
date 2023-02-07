753aaacc9bc8db5b2b265fa67406f16c5ac36a39
liuwenbinj <40383171+liuwenbinj@users.noreply.github.com>
2023-02-07 15:41:42 +0800

    Create QueryPojo.java

---
+++ src/main/com/report/entity/pojo/QueryPojo.java
@@ -0,0 +1,16 @@
+package com.report.entity.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: lwb
+ * @CreateTime: 2023-02-07  10:08
+ * @Description: 查询条件类
+ * @Version: 1.0
+ */
+@Data
+public class QueryPojo {
+    private String name;
+    private Data time;
+    private int id;
+}
