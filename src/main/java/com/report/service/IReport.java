caa13621d8951e4c74a69ebfaccd225a0de0a795
liuwenbinj <40383171+liuwenbinj@users.noreply.github.com>
2023-02-08 10:18:18 +0800

    Update IReport.java

--- src/main/java/com/report/service/IReport.java
+++ src/main/java/com/report/service/IReport.java
@@ -5,5 +5,11 @@ import com.report.entity.pojo.QueryPojo;
 import java.util.Map;
 
 public interface IReport {
+    
+    /**
+     * 查询报表1
+     * @param queryPojo 查询条件参数
+     * @return
+     */
     Map<String,String> getReport1(QueryPojo queryPojo);
 }
