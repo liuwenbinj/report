01d60c746784f1f1b704cab6d7b78cdbb697b5e4
liuwenbinj <40383171+liuwenbinj@users.noreply.github.com>
2023-02-08 10:15:46 +0800

    Update ReportApplication.java

--- src/main/java/com/report/ReportApplication.java
+++ src/main/java/com/report/ReportApplication.java
@@ -1,15 +1,35 @@
-package com.report;
+package com.report.controller;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.report.entity.common.ReturnResult;
+import com.report.entity.pojo.QueryPojo;
+import com.report.mapper.ReportMapper;
+import com.report.service.IReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@MapperScan("com.report.mapper")
-public class ReportApplication {
+import java.util.Map;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReportApplication.class, args);
+/**
+ * @Author: lwb
+ * @CreateTime: 2023-02-07  10:05
+ * @Description: 报表控制层
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("report")
+public class ReportController {
+
+    @Autowired
+    private IReport reportService;
+    @Autowired
+    private ReportMapper reportMapper;
+
+    @RequestMapping("getReport1")
+    public ReturnResult getReport1(QueryPojo queryPojo){
+        //查询数据方法
+        Map<String, String> report1 = reportService.getReport1(queryPojo);
+//        Map<String, String> report1 = reportMapper.getReport1(queryPojo);
+        return ReturnResult.success(report1, "success");
     }
-
 }
